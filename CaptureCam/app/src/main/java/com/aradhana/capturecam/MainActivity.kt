bd283f40e259041992d35fa4b035aaa3154ee221
Aradhana Jewellers <info@aradhanajewellers.com>
2026-08-16 18:57:42 +0530

    auto: save changes

--- CaptureCam/app/src/main/java/com/aradhana/capturecam/MainActivity.kt
+++ CaptureCam/app/src/main/java/com/aradhana/capturecam/MainActivity.kt
@@ -3,11 +3,13 @@ package com.aradhana.capturecam
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.hardware.camera2.CaptureResult
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
