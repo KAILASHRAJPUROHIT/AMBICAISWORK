1a628eb79a7efb96376496eb9aba7df1d9b5e214
Aradhana Jewellers <info@aradhanajewellers.com>
2026-08-19 17:17:27 +0530

    auto: save changes

--- pipeline_queue.py
+++ pipeline_queue.py
@@ -84,6 +84,7 @@ class IntakeCandidate:
                 for part in self.relative_path.parts[:-1]
             )
             and self.relative_path.suffix.casefold() in PROCESSABLE_EXTENSIONS
+            and not _ANGLE_OR_PREVIEW_SUFFIX_RE.search(self.relative_path.stem)
         )
 
 
